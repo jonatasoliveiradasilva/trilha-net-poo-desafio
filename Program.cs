a90ddd8ae8802c890e661e65e4a7a0bdaccfcf72
Jônatas Oliveira da Silva <oliveirajonatas@outlook.com.br>
2024-10-07 08:53:49 -0300

    desafio de projeto concluído

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Smartphone nokia = new Nokia(numero: "123456", modelo: "Nokia 3310", imei:"111222333444", memoria: 32);
+
+nokia.Ligar();
+nokia.ReceberLigacao();
+
+nokia.InstalarAplicativo("WhatsApp");
+
+Smartphone iphone = new Iphone(numero: "7891011", modelo: "iPhone 12", imei: "555666777888", memoria: 128);
+
+iphone.Ligar();
+iphone.ReceberLigacao();
+
+iphone.InstalarAplicativo("Instagram");
